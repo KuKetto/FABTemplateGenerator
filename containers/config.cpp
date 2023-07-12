829e78523f740bc51c7d5b4e0ac2b2691d796f11
KuKetto <domonkos115@gmail.com>
2023-07-12 22:28:31 +0200

    Added documentation to Config class

--- containers/config.cpp
+++ containers/config.cpp
@@ -5,7 +5,7 @@ unsigned int Config::get_image_count() const
     return image_count;
 }
 
-void Config::set_image_count(unsigned int new_image_count)
+void Config::set_image_count(const unsigned int& new_image_count)
 {
     image_count = new_image_count;
 }
@@ -15,7 +15,7 @@ unsigned int Config::get_normal_image_count() const
     return normal_image_count;
 }
 
-void Config::set_normal_image_count(unsigned int new_normal_image_count)
+void Config::set_normal_image_count(const unsigned int& new_normal_image_count)
 {
     normal_image_count = new_normal_image_count;
 }
@@ -25,7 +25,7 @@ unsigned int Config::get_augment_count() const
     return augment_count;
 }
 
-void Config::set_augment_count(unsigned int new_augment_count)
+void Config::set_augment_count(const unsigned int& new_augment_count)
 {
     augment_count = new_augment_count;
 }
@@ -35,7 +35,7 @@ unsigned int Config::get_augment_noise() const
     return augment_noise;
 }
 
-void Config::set_augment_noise(unsigned int new_augment_noise)
+void Config::set_augment_noise(const unsigned int& new_augment_noise)
 {
     augment_noise = new_augment_noise;
 }
@@ -45,7 +45,7 @@ unsigned int Config::get_augment_lens_blur() const
     return augment_lens_blur;
 }
 
-void Config::set_augment_lens_blur(unsigned int new_augment_lens_blur)
+void Config::set_augment_lens_blur(const unsigned int& new_augment_lens_blur)
 {
     augment_lens_blur = new_augment_lens_blur;
 }
@@ -55,7 +55,7 @@ unsigned int Config::get_augment_bilateral_blur() const
     return augment_bilateral_blur;
 }
 
-void Config::set_augment_bilateral_blur(unsigned int new_augment_bilateral_blur)
+void Config::set_augment_bilateral_blur(const unsigned int& new_augment_bilateral_blur)
 {
     augment_bilateral_blur = new_augment_bilateral_blur;
 }
@@ -65,7 +65,7 @@ unsigned int Config::get_augment_image_flip() const
     return augment_image_flip;
 }
 
-void Config::set_augment_image_flip(unsigned int new_augment_image_flip)
+void Config::set_augment_image_flip(const unsigned int& new_augment_image_flip)
 {
     augment_image_flip = new_augment_image_flip;
 }
@@ -75,7 +75,7 @@ unsigned int Config::get_augment_cut_out() const
     return augment_cut_out;
 }
 
-void Config::set_augment_cut_out(unsigned int new_augment_cut_out)
+void Config::set_augment_cut_out(const unsigned int& new_augment_cut_out)
 {
     augment_cut_out = new_augment_cut_out;
 }
@@ -85,7 +85,7 @@ unsigned int Config::get_augment_brightness() const
     return augment_brightness;
 }
 
-void Config::set_augment_brightness(unsigned int new_augment_brightness)
+void Config::set_augment_brightness(const unsigned int& new_augment_brightness)
 {
     augment_brightness = new_augment_brightness;
 }
@@ -95,7 +95,40 @@ unsigned int Config::get_augment_rgb_shift() const
     return augment_rgb_shift;
 }
 
-void Config::set_augment_rgb_shift(unsigned int new_augment_rgb_shift)
+void Config::set_augment_rgb_shift(const unsigned int& new_augment_rgb_shift)
 {
     augment_rgb_shift = new_augment_rgb_shift;
+}
+
+bool Config::validate() const
+{
+    if (image_count - normal_image_count != augment_count)
+        throw InvalidValueException("Config::validate",
+                                    "image_count - normal_image_count = augment_count",
+                                    (
+                                        "("
+                                        + std::to_string(image_count)
+                                        + " - " + std::to_string(normal_image_count)
+                                        + " != " + std::to_string(augment_count) + ")")
+                                   );
+
+    if (augment_noise + augment_lens_blur + augment_bilateral_blur
+        + augment_image_flip + augment_cut_out + augment_brightness
+        + augment_rgb_shift != augment_count)
+        throw InvalidValueException("Config::validate",
+                                    ("augment_noise + augment_lens_blur + augment_bilateral_blur"
+                                    "+ augment_image_flip + augment_cut_out + augment_brightness"
+                                     "+ augment_rgb_shift != augment_count"),
+                                    (
+                                        "("
+                                        + std::to_string(augment_noise)
+                                        + " + " + std::to_string(augment_lens_blur)
+                                        + " + " + std::to_string(augment_bilateral_blur)
+                                        + " + " + std::to_string(augment_image_flip)
+                                        + " + " + std::to_string(augment_cut_out)
+                                        + " + " + std::to_string(augment_brightness)
+                                        + " + " + std::to_string(augment_rgb_shift)
+                                        + " != " + std::to_string(augment_count) + ")")
+                                    );
+    return true;
 }
--- containers/config.h
+++ containers/config.h
@@ -1,6 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "../utils/exceptions/invalidvalueexception.h"
+
+/**
+ * @file CardPositionData.h
+ *
+ * @brief This class is responsible for storing
+ * the config what is used during generation to decide
+ * how much image should be generated.
+ *
+ * @author Domonkos Say
+ *
+ */
 
 class Config
 {
@@ -15,36 +27,126 @@ class Config
     unsigned augment_brightness;
     unsigned augment_rgb_shift;
 public:
+    /** @brief Creates an empty Config object.
+     *  This object stores the loaded config.
+     *  Use the setters to set the read values
+     *  and the getters to access them.
+     *  Use the validate function to validate a
+     *  config file.
+    */
     Config(){};
+
+    /** @brief A getter for the image count config*/
     unsigned int get_image_count() const;
-    void set_image_count(unsigned int new_image_count);
+    /** @brief A setter for the image count config
+     *
+     *  @param new_image_count: a number representing the total
+     *  number of images
+    */
+    void set_image_count(const unsigned int& new_image_count);
 
+
+    /** @brief A getter for the normal image (not augmented) count config*/
     unsigned int get_normal_image_count() const;
-    void set_normal_image_count(unsigned int new_normal_image_count);
+    /** @brief A setter for the normal image (not augmented) count config
+     *
+     *  @param new_normal_image_count: a number representing the total
+     *  number of not augmented images
+    */
+    void set_normal_image_count(const unsigned int& new_normal_image_count);
 
+
+    /** @brief A getter for the image augment count config*/
     unsigned int get_augment_count() const;
-    void set_augment_count(unsigned int new_augment_count);
+    /** @brief A setter for the image augment count config
+     *
+     *  @param new_augment_count: a number representing the total
+     *  number of augments
+    */
+    void set_augment_count(const unsigned int& new_augment_count);
 
+
+    /** @brief A getter for the noise augment count config*/
     unsigned int get_augment_noise() const;
-    void set_augment_noise(unsigned int new_augment_noise);
+    /** @brief A setter for the noise augment count config
+     *
+     *  @param new_augment_noise: a number representing the total
+     *  number of noise augment operation to be done
+    */
+    void set_augment_noise(const unsigned int& new_augment_noise);
 
+
+    /** @brief A getter for the lens blur augment count config*/
     unsigned int get_augment_lens_blur() const;
-    void set_augment_lens_blur(unsigned int new_augment_lens_blur);
+    /** @brief A setter for the lens blur augment count config
+     *
+     *  @param new_augment_lens_blur: a number representing the total
+     *  number of lens blur augment operation to be done
+    */
+    void set_augment_lens_blur(const unsigned int& new_augment_lens_blur);
 
+
+    /** @brief A getter for the bilateral blur augment count config*/
     unsigned int get_augment_bilateral_blur() const;
-    void set_augment_bilateral_blur(unsigned int new_augment_bilateral_blur);
+    /** @brief A setter for the bilateral blur augment count config
+     *
+     *  @param new_augment_bilateral_blur: a number representing the total
+     *  number of bilateral blur augment operation to be done
+    */
+    void set_augment_bilateral_blur(const unsigned int& new_augment_bilateral_blur);
 
+
+    /** @brief A getter for the image flip augment count config*/
     unsigned int get_augment_image_flip() const;
-    void set_augment_image_flip(unsigned int new_augment_image_flip);
+    /** @brief A setter for the image flip augment count config
+     *
+     *  @param new_augment_image_flip: a number representing the total
+     *  number of image flip augment operation to be done
+    */
+    void set_augment_image_flip(const unsigned int& new_augment_image_flip);
 
+
+    /** @brief A getter for the cut out augment count config*/
     unsigned int get_augment_cut_out() const;
-    void set_augment_cut_out(unsigned int new_augment_cut_out);
+    /** @brief A setter for the cut out augment count config
+     *
+     *  @param new_augment_cut_out: a number representing the total
+     *  number of cut out augment operation to be done
+    */
+    void set_augment_cut_out(const unsigned int& new_augment_cut_out);
 
+
+    /** @brief A getter for the brightness adjustion augment count config*/
     unsigned int get_augment_brightness() const;
-    void set_augment_brightness(unsigned int new_augment_brightness);
+    /** @brief A setter for the brightness adjustion augment count config
+     *
+     *  @param new_augment_brightness: a number representing the total
+     *  number of brightness adjustion augment operation to be done
+    */
+    void set_augment_brightness(const unsigned int& new_augment_brightness);
 
+
+    /** @brief A getter for the rgb shift augment count config*/
     unsigned int get_augment_rgb_shift() const;
-    void set_augment_rgb_shift(unsigned int new_augment_rgb_shift);
+    /** @brief A setter for the rgb shift augment count config
+     *
+     *  @param new_augment_rgb_shift: a number representing the total
+     *  number of rgb shift augment operation to be done
+    */
+    void set_augment_rgb_shift(const unsigned int& new_augment_rgb_shift);
+
+
+    /** @brief Checks if the input config file was valid.
+     *  It checks that the total image count is equal to
+     *  the normal image count + augment count or not.
+     *
+     *  It checks that the invidual augments adds up
+     *  to the total number of augments or not.
+     *
+     *  @throws Utils::Exceptions::InvalidValueException
+     *  if either case was false so the config file is invalid.
+    */
+    bool validate() const;
 };
 
 #endif // CONFIG_H
