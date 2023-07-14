ffe37b75444a33c83a7e8e1fe6993641c01d4572
KuKetto <domonkos115@gmail.com>
2023-07-14 17:14:24 +0200

    Added documentation for TemplateContainer

--- containers/templatecontainer.cpp
+++ containers/templatecontainer.cpp
@@ -3,19 +3,25 @@
 TemplateContainer::TemplateContainer(const std::string &directory, const unsigned int &image_count):
     image_count(image_count)
 {
+    if (!std::filesystem::exists(directory) || !std::filesystem::is_directory(directory))
+        throw PathNotFoundException("InputDataContainer::InputDataContainer", directory);
     for (const auto& entry : std::filesystem::directory_iterator(directory))
     {
         if (std::filesystem::path(entry).extension() == ".zip") {
             Image* new_template = new Image(true);
             new_template->set_file_path(entry.path().c_str());
-            unsigned int required_images = check_number_of_required_images(new_template);
+            try {
+                unsigned int required_images = check_number_of_required_images(new_template);
 
-            if (required_images <= image_count) {
-                templates.push_back
-                    (QPair<Image*, unsigned int>(new_template, required_images)
-                );
-                if (required_images > maximum_required_images) maximum_required_images = required_images;
-            } else delete new_template;
+                if (required_images <= image_count) {
+                    templates.push_back(QPair<Image*, unsigned int>(new_template, required_images));
+                    if (required_images > maximum_required_images) maximum_required_images = required_images;
+                } else delete new_template;
+
+            } catch (InvalidTemplateException& ex) {
+                qDebug() << ex.what();
+                delete new_template;
+            }
         }
     }
 }
@@ -31,7 +37,11 @@ TemplateContainer::~TemplateContainer()
 
 unsigned int TemplateContainer::check_number_of_required_images(Image *template_image)
 {
-    template_image->open();
+    try {
+        template_image->open();
+    } catch (std::exception& ex) {
+        throw InvalidTemplateException("TemplateContainer::check_number_of_required_images", ex.what());
+    }
     unsigned int required_size = template_image->get_card_positions_size();
     template_image->close();
 
@@ -40,11 +50,17 @@ unsigned int TemplateContainer::check_number_of_required_images(Image *template_
 
 Image *TemplateContainer::select_a_template()
 {
+    if (templates.empty()) throw NoValidInputsLeftException("TemplateContainer::select_a_template", "");
     return templates.at(Random::generate_integer(0, templates.size() - 1)).first;
 }
 
 void TemplateContainer::on_image_deleted()
 {
+    if (templates.empty()) throw BadUsageException("TemplateContainer::on_image_deleted",
+                                "There were no templates left meaning all images should "
+                                "have been finished and the generation should have been stopped. "
+                                "Or there was no template used which only requires the minimum "
+                                "amount of input images (1).");
     image_count--;
     if (maximum_required_images <= image_count) return;
 
--- containers/templatecontainer.h
+++ containers/templatecontainer.h
@@ -3,6 +3,20 @@
 
 #include "image.h"
 #include "../utils/random.h"
+#include "../utils/exceptions/invalidtemplateexception.h"
+#include "../utils/exceptions/novalidinputsleftexception.h"
+#include "../utils/exceptions/badusageexception.h"
+
+/**
+ * @file TemplateContainer.h
+ *
+ * @brief This class is responsible for storing
+ * how much operation is left on an image from
+ * a given type.
+ *
+ * @author Domonkos Say
+ *
+ */
 
 class TemplateContainer
 {
@@ -10,12 +24,51 @@ class TemplateContainer
     unsigned int maximum_required_images = 0;
     unsigned int image_count;
 
+    /** @brief A helper function to simplify the code in the
+     *  constructor. It requires reading in the template file
+     *  to define the required value. Afterwards it closes
+     *  the image.
+     *
+     *  @param template_image: A pointer to the template Image
+     *  object in which the check is requested.
+     *
+     *  @throws InvalidTemplateException if the template's reading
+     *  process fails
+     */
     unsigned int check_number_of_required_images(Image* template_image);
 public:
+    /** @brief Constructor, creates a new TemplateContainer object.
+     *  Requires a directory of templates.
+     *
+     *  @param directory: Directory path where the templates are located
+     *  @param image_count: the number of input images that will be used
+     *  during generation (the number of images loaded by InputDataContainer)
+     *
+     *  @throws Utils::Directory::PathNotFoundException if the given directory
+     *  path is invalid
+     */
     TemplateContainer(const std::string& directory, const unsigned int& image_count);
+
+    /** @brief Destructs the allocated data in templates QVector
+     *  by calling Image::close methods on the Image objects
+     *  then deleting them.
+     */
     ~TemplateContainer();
 
+    /** @brief Selects a template to be use by Generation::generate.
+     *  Selects a random number as an index of templates QVector.
+     *
+     *  @throws Utils::Exceptions::NoValidInputsLeftException if
+     *  no templates are left. In this case the generation finishes.
+    */
     Image* select_a_template();
+
+    /** @brief To be called after an image is removed from InputDataContainer.
+     *  Decreases the image counter. Removes all templates that requires more
+     *  input image than what left.
+     *
+     *  @throws Utils::Exceptions::BadUsageException no more templates left
+     */
     void on_image_deleted();
 };
 
